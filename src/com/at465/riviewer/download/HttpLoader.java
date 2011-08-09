aad5f49f38dcbb0b058ca777c46c3b6f3f9002b5
Antony Tran <antonyt@gmail.com>
2011-08-09 17:29:57 +0100

    Added synchronized block to prevent exceptions caused by opening multiple connections simultaneously

--- src/com/at465/riviewer/download/HttpLoader.java
+++ src/com/at465/riviewer/download/HttpLoader.java
@@ -11,12 +11,11 @@ import android.content.Context;
 import android.support.v4.content.AsyncTaskLoader;
 
 public class HttpLoader<D> extends AsyncTaskLoader<D> {
-    
+
     private static final HttpClient httpClient = new GzipHttpClient();
     private ResponseHandler<D> handler;
     private HttpUriRequest request;
-    
-    
+
     public HttpLoader(Context context, HttpUriRequest request, ResponseHandler<D> handler) {
 	super(context);
 	this.request = request;
@@ -26,13 +25,15 @@ public class HttpLoader<D> extends AsyncTaskLoader<D> {
     @Override
     public D loadInBackground() {
 	try {
-	    return httpClient.execute(request, handler);
+	    synchronized (httpClient) {
+		return httpClient.execute(request, handler);
+	    }
 	} catch (ClientProtocolException e) {
 	    e.printStackTrace();
 	} catch (IOException e) {
 	    e.printStackTrace();
 	}
-	
+
 	return null;
     }
 
