77e707788ef3e519c51c166c2e37e346f857bbe4
Antony Tran <antonyt@gmail.com>
2011-08-10 00:14:35 +0100

    Scale by screen density

--- src/com/at465/riviewer/fragment/AsyncImageFragment.java
+++ src/com/at465/riviewer/fragment/AsyncImageFragment.java
@@ -2,6 +2,7 @@ package com.at465.riviewer.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,7 +15,16 @@ public class AsyncImageFragment extends Fragment {
     private static final String BASE_URL = "http://api.imgur.com/%sl%s";
     private Image image;
     private WebView webView;
+    private float density;
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+	DisplayMetrics dm = new DisplayMetrics();
+	getActivity().getWindowManager().getDefaultDisplay().getMetrics(dm);
+	this.density = dm.density;
+    }
+    
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 	webView = new WebView(getActivity());
@@ -26,12 +36,15 @@ public class AsyncImageFragment extends Fragment {
     }
 
     
-    public void loadImage(Image image) {	
-	this.image = image;
+    public void loadImage(Image image) {
 	webView.stopLoading();
+	this.image = image;
 	String url = String.format(BASE_URL, image.getHash(), image.getExt());
-	float width = webView.getWidth() / 1.5f;
-	float height = webView.getHeight() / 1.5f;
+	
+
+	
+	float width = webView.getWidth() / density;
+	float height = webView.getHeight() / density;
 	String style = "<head><style>* {margin:0;padding:0;}</style></head>";
 	String html = style + "<body><img id='image' src='" + url + "' style='position:absolute; top:0; bottom:0; left:0; right:0; margin:auto; max-width:" + width + "px; max-height:" + height + "'/></body>";
 	Log.d("AsyncImageFragment", "loadImage " + html);
