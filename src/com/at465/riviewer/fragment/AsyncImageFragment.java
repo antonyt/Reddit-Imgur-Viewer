e47f775ed3c57320ad05d27b039b0d1213119457
Antony Tran <antonyt@gmail.com>
2011-08-10 18:27:14 +0100

    Using local html template. Rotation now works.

--- src/com/at465/riviewer/fragment/AsyncImageFragment.java
+++ src/com/at465/riviewer/fragment/AsyncImageFragment.java
@@ -1,5 +1,6 @@
 package com.at465.riviewer.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.DisplayMetrics;
@@ -13,6 +14,7 @@ import com.at465.riviewer.deserialise.Image;
 
 public class AsyncImageFragment extends Fragment {
     private static final String BASE_URL = "http://api.imgur.com/%s%s";
+    private static final String TEMPLATE_URL = "file:///android_asset/image_template.html";
     private Image image;
     private WebView webView;
     private float density;
@@ -27,26 +29,35 @@ public class AsyncImageFragment extends Fragment {
     
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-	webView = new WebView(getActivity());
-	webView.setBackgroundColor(0);
+	webView = new WebView(getActivity()) {
+	    @Override
+	    protected void onSizeChanged(int w, int h, int ow, int oh) {
+		super.onSizeChanged(w, h, ow, oh);
+		if (image != null && w > 0) {
+		    loadImage(image);
+		}
+	    };
+	};
+	webView.setBackgroundColor(Color.BLACK);
 	webView.getSettings().setBuiltInZoomControls(true);
 	webView.getSettings().setJavaScriptEnabled(true);
 	webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+	webView.loadUrl(TEMPLATE_URL);
 	return webView;
     }
 
     
     public void loadImage(Image image) {
 	webView.stopLoading();
-	webView.clearView();
 	this.image = image;
 	String url = String.format(BASE_URL, image.getHash(), image.getExt());
-
 	float width = webView.getWidth() / density;
 	float height = webView.getHeight() / density;
-	String style = "<head><style>* {margin:0;padding:0;}</style></head>";
-	String html = style + "<body><img id='image' src='" + url + "' style='position:absolute; top:0; bottom:0; left:0; right:0; margin:auto; max-width:" + width + "px; max-height:" + height + "'/></body>";
-	Log.d("AsyncImageFragment", "loadImage " + html);
-	webView.loadDataWithBaseURL(null, html, "text/html", "utf-8", null);
+	String s = "javascript: loadImage('%s', %s, %s);";
+	s = String.format(s, url, width, height);
+	Log.d("AsyncImageFragment", "loadImage " + s);
+	webView.loadUrl(s);
     }
+    
+    
 }
